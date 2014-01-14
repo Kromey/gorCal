699d24c4672a1a183f204080c3e3deab59348fe0
Kromey <travisvz@gmail.com>
2014-01-14 16:38:59 -0600

    Removed debug statements

--- gorDate.js
+++ gorDate.js
@@ -9,8 +9,6 @@ function gorDate(obj)
 	var now = new Date();
 	var days = getDaysToDate(now);
 	var equinox = getDaysToDate(new Date(now.getFullYear(), 2, 20));
-	console.log('Today is '+days+' days since Jan 1');
-	console.log('The Equinox is '+equinox+' days since Jan 1');
 
 	var year = now.getFullYear();
 	if(days < equinox)
@@ -62,7 +60,6 @@ function getDaysToDate(now)
 	var start = new Date(now.getFullYear(), 0, 1, 0, 0, 0);
 	var end = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0);
 	var diff = end - start;
-	console.log('There are '+diff+' microseconds between '+start+' and '+end);
 
 	return Math.round(diff / (1000 * 60 * 60 * 24));
 }
